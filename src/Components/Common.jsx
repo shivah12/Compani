956b73da136faacf8552a893f486349ab73307a1
Shreeti <114981873+shivah12@users.noreply.github.com>
2024-02-12 15:23:08 +0530

    Update Common.jsx

--- src/Components/Common.jsx
+++ src/Components/Common.jsx
@@ -18,33 +18,32 @@ const Common = ({
     return (
         <>
             <section id="header" className="d-flex align-items-center">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-10 mx-auto">
-                        <div className="row">
-                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column" data-aos="fade-up">
-                                <h1> 
-                                     {name}
-                                     {isCompName ? <strong className="brand-name"> {compName}</strong> : ""}
-                                    
-                                </h1>
-                                <h2 className="my-3">
-                                    We are the team of talented developer making websites
-                                </h2>
-                                <div className="mt-3">
-                                    <NavLink to={visit} className="btn-get-started ">
-                                        {btnname}
-                                    </NavLink>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="row">
+                                <div className="col-lg-6 order-2 order-lg-1 d-flex justify-content-center flex-column" data-aos="fade-up">
+                                    <h1> 
+                                        {name}
+                                        {isCompName ? <strong className="brand-name"> {compName}</strong> : ""}
+                                    </h1>
+                                    <h2 className="my-3">
+                                        We are the team of talented developer making websites
+                                    </h2>
+                                    <div className="mt-3">
+                                        <NavLink to={visit} className="btn-get-started">
+                                            {btnname}
+                                        </NavLink>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="col-lg-6 order-1 order-lg-2 header-image radial-gradient(circle, #1E1857, #C5C5C6, #19162D)" data-aos="fade-left">
-                                <img src={imgsrc} className="img-fluid animated" alt="Home Img"/>
+                                <div className="col-lg-6 order-1 order-lg-2 header-image radial-gradient(circle, #1E1857, #C5C5C6, #19162D)" data-aos="fade-left">
+                                    <img src={imgsrc} className="img-fluid animated" alt="Home Img"/>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </section>
         </>
     )
